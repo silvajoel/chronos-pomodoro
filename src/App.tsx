a202d9a8fbaf44532852462be83905e74c476066
unknown <silvajoel06@gmail.com>
2026-05-29 12:00:04 -0300

    feat: Add Logo components and folder organization modules.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
 // PascalCase
 // App
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
+
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Heading } from "./components/Heading";
-import { TimerIcon } from "lucide-react";
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet. Eos corporis voluptatem et consequuntur
-        officiis 33 blanditiis omnis. Sit Quis voluptatum sed dignissimos
-        ducimus ea Quis quas et esse dolor est galisum architecto qui
-        reprehenderit rerum. Est dolores numquam cum vero totam aut nesciunt
-        aliquam et consequuntur repudiandae et nihil mollitia. Ut consequatur
-        accusamus ea libero eveniet sit totam beatae. Cum dolorem quos sit
-        praesentium obcaecati non Quis totam ad accusantium nobis aut magnam
-        delectus et ipsa nobis. Gerador de Conteúdo Eos eveniet atque cum culpa
-        quisquam non omnis fuga est soluta quis qui assumenda consequatur aut
-        consequatur internos aut quos expedita. Eos ipsam tempora et nemo
-        asperiores et animi repudiandae. Cum autem recusandae quo dolore aliquam
-        est velit saepe non quod minima cum amet omnis et dolor repudiandae. Qui
-        omnis eius sit quod reiciendis et nobis totam quo voluptatum assumenda.
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
